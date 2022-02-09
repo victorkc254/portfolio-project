cd3e6af83f2358fc95d0dde66bb518ad106c4a1f
victorkc254 <cheruiyotvictor@zetech.ac.ke>
2022-02-09 13:09:02 +0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import logo from "../Logo12.png";
+//Font awesome imports//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={logo} alt="" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <FontAwesomeIcon icon="{faBars}" style={{ color: "#fff" }} />
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                about
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                experience
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                contacts
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

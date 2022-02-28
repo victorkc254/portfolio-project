7753d869cdc69b102aad54bd9509af55c3d7d2a7
Victor kiprono <vickiprono50@gmail.com>
2022-02-28 16:50:56 +0300

    Experience component completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Services from "./components/Services";
+import Experience from "./components/Experience";
 
 function App() {
   return (
-    <>  
-    
+    <>
+      
       <Navbar />
       <Header />
       <About />
+      <Services />
+      <Experience />
     </>
   );
 }
